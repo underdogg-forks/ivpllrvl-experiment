892b4868bcd1bd278ff7a7efd1d75baeafddb9b7
Niels Drost <47660417+nielsdrost7@users.noreply.github.com>
2025-11-01 14:11:00 +0100

    Refactor ReportsController and update namespaces

--- Modules/Core/Controllers/ReportsController.php
+++ Modules/Core/Controllers/ReportsController.php
@@ -1,13 +1,129 @@
 <?php
 
-namespace Modules\Core\Controllers;
+namespace Modules\Core\Models;
 
-class ReportsController
+use AllowDynamicProperties;
+use Illuminate\Http\Request;
+use Modules\Core\Controllers\AdminController;
+use Modules\Reports\Controllers\ReportsService;
+
+#[AllowDynamicProperties]
+class ReportsController extends AdminController
 {
-    /** @legacy-file application/modules/reports/controllers/Reports.php */
-    public function index(): \Illuminate\View\View
+    /**
+     * Initialize the ReportsController.
+     */
+    public function __construct()
     {
-        // Financial reports and analytics
-        return view('core::reports_index');
+        parent::__construct();
+    }
+
+    /**
+     * @originalName salesByClient
+     *
+     * @originalFile ReportsController.php
+     */
+    public function salesByClient(Request $request): \Illuminate\Contracts\View\View
+    {
+        if ($request->input('btn_submit')) {
+            $results = (new ReportsService())->salesByClient($request->input('from_date'), $request->input('to_date'));
+            $data    = [
+                'results'   => $results,
+                'from_date' => $request->input('from_date'),
+                'to_date'   => $request->input('to_date'),
+            ];
+            // TODO: Use Laravel PDF package to generate PDF from view
+            // PDF::loadView('reports.sales_by_client', $data)->download('sales_by_client.pdf');
+            // return response()->download(...);
+        }
+
+        return view('reports.sales_by_client_index');
+    }
+
+    /**
+     * @originalName invoicesPerClient
+     *
+     * @originalFile ReportsController.php
+     */
+    public function invoicesPerClient(Request $request): \Illuminate\Contracts\View\View
+    {
+        if ($request->input('btn_submit')) {
+            $results = (new ReportsService())->invoicesPerClient($request->input('from_date'), $request->input('to_date'));
+            $data    = [
+                'results'   => $results,
+                'from_date' => $request->input('from_date'),
+                'to_date'   => $request->input('to_date'),
+            ];
+            // TODO: Use Laravel PDF package to generate PDF from view
+        }
+
+        return view('reports.invoices_per_client_index');
+    }
+
+    /**
+     * @originalName paymentHistory
+     *
+     * @originalFile ReportsController.php
+     */
+    public function paymentHistory(Request $request): \Illuminate\Contracts\View\View
+    {
+        if ($request->input('btn_submit')) {
+            $results = (new ReportsService())->paymentHistory($request->input('from_date'), $request->input('to_date'));
+            $data    = [
+                'results'   => $results,
+                'from_date' => $request->input('from_date'),
+                'to_date'   => $request->input('to_date'),
+            ];
+            // TODO: Use Laravel PDF package to generate PDF from view
+        }
+
+        return view('reports.payment_history_index');
+    }
+
+    /**
+     * @originalName invoiceAging
+     *
+     * @originalFile ReportsController.php
+     */
+    public function invoiceAging(Request $request): \Illuminate\Contracts\View\View
+    {
+        if ($request->input('btn_submit')) {
+            $results = (new ReportsService())->invoiceAging();
+            $data    = [
+                'results' => $results,
+            ];
+            // TODO: Use Laravel PDF package to generate PDF from view
+        }
+
+        return view('reports.invoice_aging_index');
+    }
+
+    /**
+     * Display the Sales by Year report page and prepare report data when the form is submitted.
+     *
+     * When the request includes 'btn_submit', prepares report data (results, from_date, to_date)
+     * from the request inputs for rendering or PDF generation.
+     *
+     * @return \Illuminate\Contracts\View\View the view for the sales by year report
+     */
+    public function salesByYear(Request $request): \Illuminate\Contracts\View\View
+    {
+        if ($request->input('btn_submit')) {
+            $results = (new ReportsService())->salesByYear(
+                $request->input('from_date'),
+                $request->input('to_date'),
+                $request->input('minQuantity'),
+                $request->input('maxQuantity'),
+                $request->input('checkboxTax')
+            );
+            $data = [
+                'results'   => $results,
+                'from_date' => $request->input('from_date'),
+                'to_date'   => $request->input('to_date'),
+            ];
+            // TODO: Use Laravel PDF package to generate PDF from view
+        }
+
+        return view('reports.sales_by_year_index');
     }
 }
