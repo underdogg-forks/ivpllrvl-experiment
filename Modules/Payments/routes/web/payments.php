f21cbe2790b8894a8865d2c742c0c0fe898176ad
Niels Drost <47660417+nielsdrost7@users.noreply.github.com>
2025-11-02 07:44:30 +0100

    Update payment routes to use PaymentsAjaxController

--- Modules/Payments/routes/web/payments.php
+++ Modules/Payments/routes/web/payments.php
@@ -1,12 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\Payments\Controllers\AjaxController;
+use Modules\Payments\Controllers\PaymentsAjaxController;
 use Modules\Payments\Controllers\PaymentsController;
 
 Route::middleware('web')->group(function () {
-    Route::get('payments/add', [AjaxController::class, 'add'])->name('payments.add');
-    Route::get('payments/modal-add-payment', [AjaxController::class, 'modalAddPayment'])->name('payments.modal-add-payment');
+    Route::get('payments/add', [PaymentsAjaxController::class, 'add'])->name('payments.add');
+    Route::get('payments/modal-add-payment', [PaymentsAjaxController::class, 'modalAddPayment'])->name('payments.modal-add-payment');
     Route::get('payments', [PaymentsController::class, 'index'])->name('payments.index');
     Route::get('payments/form', [PaymentsController::class, 'form'])->name('payments.form');
     Route::get('payments/online-logs', [PaymentsController::class, 'onlineLogs'])->name('payments.online-logs');
