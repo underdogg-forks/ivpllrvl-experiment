9eb37e0c4d2cc8d227934c933a52763b2d29dbf1
Niels Drost <47660417+nielsdrost7@users.noreply.github.com>
2025-11-02 07:39:47 +0100

    Refactor payment routes with middleware grouping

--- Modules/Payments/routes/web/payments.php
+++ Modules/Payments/routes/web/payments.php
@@ -1,34 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Modules\Payments\Controllers\PaymentMethodsController;
+use Modules\Payments\Controllers\AjaxController;
 use Modules\Payments\Controllers\PaymentsController;
 
-/*
-|--------------------------------------------------------------------------
-| Payments Web Routes
-|--------------------------------------------------------------------------
-*/
-
-// Index routes
-Route::get('/payments/index', [PaymentsController::class, 'index'])->name('payments.index');
-Route::get('/payment_methods/index', [PaymentMethodsController::class, 'index'])->name('payment_methods.index');
-
-// Form routes (GET to display form)
-Route::get('/payments/form', [PaymentsController::class, 'form'])->name('payments.form');
-Route::get('/payments/form/{id}', [PaymentsController::class, 'form']);
-Route::get('/payment_methods/form', [PaymentMethodsController::class, 'form'])->name('payment_methods.form');
-Route::get('/payment_methods/form/{id}', [PaymentMethodsController::class, 'form']);
-
-// POST routes for create/update
-Route::post('/payments/form', [PaymentsController::class, 'form']);
-Route::post('/payments/form/{id}', [PaymentsController::class, 'form']);
-Route::post('/payment_methods/form', [PaymentMethodsController::class, 'form']);
-Route::post('/payment_methods/form/{id}', [PaymentMethodsController::class, 'form']);
-
-// Delete routes (POST for safety)
-Route::post('/payments/delete/{id}', [PaymentsController::class, 'delete'])->name('payments.delete');
-Route::post('/payment_methods/delete/{id}', [PaymentMethodsController::class, 'delete'])->name('payment_methods.delete');
-
-// AJAX routes
-Route::get('/payments/online_logs', [PaymentsController::class, 'onlineLogs'])->name('payments.online_logs');
+Route::middleware('web')->group(function () {
+    Route::get('payments/add', [AjaxController::class, 'add'])->name('payments.add');
+    Route::get('payments/modal-add-payment', [AjaxController::class, 'modalAddPayment'])->name('payments.modal-add-payment');
+    Route::get('payments', [PaymentsController::class, 'index'])->name('payments.index');
+    Route::get('payments/form', [PaymentsController::class, 'form'])->name('payments.form');
+    Route::get('payments/online-logs', [PaymentsController::class, 'onlineLogs'])->name('payments.online-logs');
+    Route::get('payments/delete', [PaymentsController::class, 'delete'])->name('payments.delete');
+});
