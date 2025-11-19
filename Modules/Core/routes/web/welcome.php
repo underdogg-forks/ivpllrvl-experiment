65d2f6d5f2d2067db6eab7f308ddbedaecedbcdf
Niels Drost <nielsdrost7@gmail.com>
2025-11-19 12:58:07 +0100

    moved uploads to storage directory

--- Modules/Core/routes/web/welcome.php
+++ Modules/Core/routes/web/welcome.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 use Modules\Core\Controllers\WelcomeController;
 
 Route::middleware('web')->group(function () {
-    Route::get('welcome', [WelcomeController::class, 'index'])->name('welcome.index');
+    Route::get('/', [WelcomeController::class, 'index'])->name('welcome.index');
 });
